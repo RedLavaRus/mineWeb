c1d8e6d6b6a8319e2cec2c792d1922e75fadff2d
RedLavaRus <github@redlava.ru>
2020-02-01 22:47:04 +0300

    0.0.1

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php 
-phpinfo();
+<?php
+include_once "core/core.php";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="/thems/css/style.css">
+</head>
+<body>
+
+</body>
+</html>
